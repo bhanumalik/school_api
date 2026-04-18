9dacf57809f32030ba6bdaf399a2a800af147a2a
bhanumalik <bhanumalik626@gmail.com>
2026-04-18 22:19:07 +0530

    added db port

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
-const mysql = require("mysql2");
-require("dotenv").config();
-
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,   // ✅ ADD THIS
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
